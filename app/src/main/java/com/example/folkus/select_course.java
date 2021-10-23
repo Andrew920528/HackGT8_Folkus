05a2dcb87e45a139f42cb4c703be6b3cb300eccd
Andrew920528 <andrewhsu.0528@gmail.com>
2021-10-23 12:52:14 -0400

    rename

--- app/src/main/java/com/example/folkus/select_course.java
+++ app/src/main/java/com/example/folkus/select_course.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class slect_course extends AppCompatActivity {
+public class select_course extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
