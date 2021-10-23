e47fd477b322d3a7e29bfb5d8a9faf7c98754bca
Karan-Patel15 <knpatel215@gmail.com>
2021-10-23 14:43:05 -0400

    added a continue button to main activity to take you to set timer page

--- app/src/main/java/com/example/folkus/MainActivity.java
+++ app/src/main/java/com/example/folkus/MainActivity.java
@@ -2,16 +2,22 @@ package com.example.folkus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Spinner mySpinner = (Spinner) findViewById(R.id.dropdown);
 
@@ -19,6 +25,19 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.ClassList));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
-    }
 
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSetTimerActivity();
+            }
+        });
+
+    }
+    public void openSetTimerActivity() {
+        Intent intent = new Intent(this, set_timer.class );
+        startActivity(intent);
+    }
 }
