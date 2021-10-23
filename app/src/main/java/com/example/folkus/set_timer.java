c6df41b387db1df19a51024790a918674dc6ac00
Andrew920528 <andrewhsu.0528@gmail.com>
2021-10-23 12:48:03 -0400

    add different pages

---
+++ app/src/main/java/com/example/folkus/set_timer.java
@@ -0,0 +1,14 @@
+package com.example.folkus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class set_timer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_timer);
+    }
+}
