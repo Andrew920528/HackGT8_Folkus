72a64b863b98750c9b7df5c52ce8994fe33e093c
Andrew920528 <andrewhsu.0528@gmail.com>
2021-10-23 12:51:29 -0400

    rename

--- app/src/main/java/com/example/folkus/select_course.java
+++ app/src/main/java/com/example/folkus/select_course.java
@@ -9,6 +9,6 @@ public class slect_course extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_slect_course);
+        setContentView(R.layout.activity_select_course);
     }
 }
